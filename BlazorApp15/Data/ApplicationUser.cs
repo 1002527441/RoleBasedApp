8c660772a2260c1c7c0392dcebe509a51212dc25
红林 张 <1002527441@qq.com>
2020-03-26 07:41:37 +0800

    new roles

---
+++ BlazorApp15/Data/ApplicationUser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp15.Data
+{
+    public class ApplicationUser
+    {
+        public string FullName { get; set; }
+        public string Email { get; set; }
+
+        public string UserName { get; set; }
+    }
+}
